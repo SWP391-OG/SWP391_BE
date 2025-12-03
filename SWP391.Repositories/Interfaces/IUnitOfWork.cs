b972a9f9043610f1335bf0bea84991575afc8122
minhquang932004 <minhquang932004@gmail.com>
2025-12-03 09:30:43 +0700

    Adding Unit Of Work

---
+++ SWP391.Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace SWP391.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        //PromotionsQuangNmRepository PromotionsQuangNmRepository { get; }
+        //PromotionUsageQuangNmRepository PromotionUsageQuangNmRepository { get; }
+        //SystemUserAccountRepository SystemUserAccountRepository { get; }
+
+
+        int SaveChangesWithTransaction();
+        Task<int> SaveChangesWithTransactionAsync();
+    }
+}
